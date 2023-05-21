06f3ad2ec4c9edf11d6047d80f2b11fa962c2442
stringer.top <stringer.top@gmail.com>
2023-05-21 09:48:02 +0330

    Change localhost and 127.0.0.1 to boxi.local

--- src/main/java/com/boxi/PriceList/repo/ServiceRepository.java
+++ src/main/java/com/boxi/PriceList/repo/ServiceRepository.java
@@ -31,7 +31,6 @@ public interface ServiceRepository extends JpaRepository<Services, Long>, JpaSpe
             "  FROM TBL_PRICELISTDETAIL tp1\n" +
             " INNER JOIN TBL_PRICELIST otp\n" +
             "    ON TP1.FK_PRICELIST_ID = OTP.PK_PRICELIST_ID\n" +
-            "\n" +
             " WHERE otp.ISDELETED = 0 and tp1.isdeleted=0 and otp.isactive=1 and tp1.isactive=1\n" +
             "   AND tp1.FK_PRODUCT_ID IN\n" +
             "       (SELECT tp.PK_PRODUCT_ID\n" +
@@ -40,5 +39,4 @@ public interface ServiceRepository extends JpaRepository<Services, Long>, JpaSpe
             "            ON TU.FK_CHILD_ID = tp.PK_PRODUCT_ID\n" +
             "         WHERE tu.FK_PARENT_ID = ?1 AND ?2 BETWEEN otp.validdatefrom AND otp.validdateto)",nativeQuery = true)
     List<SuggestDetailServiceInfDto> getsuggestDetails(Long productId , Date date);
-
 }
