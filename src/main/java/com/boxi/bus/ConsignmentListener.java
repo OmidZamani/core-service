d0ebf536c693055d35596c729a7738db3f2b2eee
ali-moj <ali-mojtehedy@yandex.com>
2023-05-31 00:35:37 +0330

    add logstash config

---
+++ src/main/java/com/boxi/bus/ConsignmentListener.java
@@ -0,0 +1,24 @@
+package com.boxi.bus;
+
+import com.boxi.transport.payload.dto.BagDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Slf4j
+public class ConsignmentListener {
+    @KafkaListener(
+     id="bags-saga-core"    ,
+     topics="bags-saga" ,
+     containerGroup = "a",
+     concurrency = "3"
+    )
+    @Transactional
+    public void listen(BagDto dto){
+        log.info("{}",dto);
+    }
+
+}
