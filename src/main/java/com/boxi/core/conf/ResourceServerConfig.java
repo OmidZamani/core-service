bd48ae002c6afa11a8c2600b30f61a9b0f7a6bf1
ali-moj <ali-mojtehedy@yandex.com>
2023-08-12 12:53:54 +0330

    changed confs

--- src/main/java/com/boxi/core/conf/ResourceServerConfig.java
+++ src/main/java/com/boxi/core/conf/ResourceServerConfig.java
@@ -32,45 +32,12 @@ private final JwtAuthConverter jwtAuthConverter;
         http.oauth2ResourceServer()
                 .jwt()
                 .jwtAuthenticationConverter(jwtAuthConverter);
-   //     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.cors().and().csrf().disable();
         return http.build();
     }
 
 
-
-/*    @Bean
-    public JwtDecoder jwtDecoder() {
-        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri("http://boxi.local:8080/realms/hubRealm/protocol/openid-connect/certs").build();
-        jwtDecoder.setJwtValidator(JwtValidators.createDefaultWithIssuer("http://boxi.local:8080/realms/hubRealm"));
-        return jwtDecoder;
-    }
-
-
-    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(new RealmRoleConverter());
-        return jwtConverter;
-    }*/
-
-/*
-    private Converter<OAuth2ResourceServerProperties.Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(new RealmRoleConverter());
-        return jwtConverter;
-    }
-*/
-
-/*    private JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(new RealmRoleConverter());
-        return jwtConverter;
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return JwtDecoders.fromIssuerLocation(issuerUri);
-    }*/
 
 
 }
--- src/main/java/com/boxi/feign/CountryDevisionClient.java
+++ src/main/java/com/boxi/feign/CountryDevisionClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name="core-service",url = "http://boxi.local:40000/resource-api/countryDevision")
+@FeignClient(name="core-service",url = "http://boxi:40000/resource-api/countryDevision")
 public interface CountryDevisionClient {
     @GetMapping()
     public List<CountryDevisionDto> getItems(@RequestParam Long id);
