917f2d7b33356acb815559d8170457eb6b82bde6
ali-moj <ali-mojtehedy@yandex.com>
2023-10-16 19:43:21 +0300

    ssl omit internal and added feign to profile

--- src/main/java/com/boxi/feign/CountryDevisionClient.java
+++ src/main/java/com/boxi/feign/CountryDevisionClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name="core-service",url = "http://boxi:40000/resource-api/countryDevision")
+@FeignClient(name="core-service",url = "${feign.client.url}/resource-api/countryDevision")
 public interface CountryDevisionClient {
     @GetMapping()
     public List<CountryDevisionDto> getItems(@RequestParam Long id);
--- src/main/java/com/boxi/feign/CustomerClient.java
+++ src/main/java/com/boxi/feign/CustomerClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "CustomerClient", url = "http://boxi:40000/customer-api")
+@FeignClient(name = "CustomerClient", url = "${feign.client.url}/customer-api")
 public interface CustomerClient {
 
     @GetMapping("/customer/findbyid/{id}")
