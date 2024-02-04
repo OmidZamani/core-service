a0a144db6f2c5b15f80545b824900cf44621b87d
stringer.top <stringer.top@gmail.com>
2024-02-04 19:51:50 +0330

    bug fix create therdParty

--- src/main/java/com/boxi/transport/payload/converter/VehicleConverter.java
+++ src/main/java/com/boxi/transport/payload/converter/VehicleConverter.java
@@ -154,15 +154,16 @@ abstract class VehicleSelectConverter implements VehicleConverter {
     public VehicleDto fromModelToDto(Vehicle vehicle) {
         VehicleDto vehicleDto = vehicleConverter.fromModelToDto(vehicle);
 
-        if (vehicleDto != null && vehicleDto.getFixedDriverId() != null) {
-            DriverDto driverDto = driverClient.findbyIdbyid(vehicleDto.getFixedDriverId());
-            vehicleDto.setFixedDriverName(driverDto.getName());
+        if(vehicleDto != null) {
+            if (vehicleDto != null && vehicleDto.getFixedDriverId() != null) {
+                DriverDto driverDto = driverClient.findbyIdbyid(vehicleDto.getFixedDriverId());
+                vehicleDto.setFixedDriverName(driverDto.getName());
+            }
+            if (vehicleDto != null && vehicleDto.getFirstDriverId() != null) {
+                DriverDto driverDto = driverClient.findbyIdbyid(vehicleDto.getFirstDriverId());
+                vehicleDto.setFirstDriverName(driverDto.getName());
+            }
         }
-        if (vehicleDto != null && vehicleDto.getFirstDriverId() != null) {
-            DriverDto driverDto = driverClient.findbyIdbyid(vehicleDto.getFirstDriverId());
-            vehicleDto.setFirstDriverName(driverDto.getName());
-        }
-
 
         return vehicleDto;
     }
