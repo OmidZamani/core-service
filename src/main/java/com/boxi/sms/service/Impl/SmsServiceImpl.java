72dd90ba056b56db2ae88bd7b3236f43eafcfb57
stringer.top <stringer.top@gmail.com>
2024-02-10 17:20:50 +0330

    add sms send  active

--- src/main/java/com/boxi/sms/service/Impl/SmsServiceImpl.java
+++ src/main/java/com/boxi/sms/service/Impl/SmsServiceImpl.java
@@ -1,15 +1,12 @@
 package com.boxi.sms.service.Impl;
 
 
-import com.boxi.sms.payload.dto.SendSMS;
 import com.boxi.sms.payload.dto.SendSmsDto;
 import com.boxi.sms.payload.dto.SmsDto;
 import com.boxi.sms.service.SmsService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.shaded.json.JSONObject;
-import org.aspectj.bridge.IMessage;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -33,6 +30,9 @@ public class SmsServiceImpl implements SmsService {
     @Value("${app.sms.user.from}")
     private String smsFrom;
 
+    @Value("${sms_send_status}")
+    private Boolean smsStatus;
+
     final static String resourceUrl = "https://media.sms24.ir/SMSInOutBox/Send";
 
     private SmsDto sendSMS(SendSmsDto dto) {
@@ -43,19 +43,24 @@ public class SmsServiceImpl implements SmsService {
         jsonObject.put("UserName", smsUserName);
         jsonObject.put("Password", smsPass);
         jsonObject.put("From", smsFrom);
-        if(dto.getPhoneNumber().substring(0,2).equals("00"))
-            dto.setPhoneNumber(dto.getPhoneNumber().substring(1,11));
+        if (dto.getPhoneNumber().substring(0, 2).equals("00"))
+            dto.setPhoneNumber(dto.getPhoneNumber().substring(1, 11));
         jsonObject.put("To", dto.getPhoneNumber());
         jsonObject.put("Message", dto.getMessage());
-       // System.out.println(">>>>>>>>>>>>"+ dto.getMessage());
-        ResponseEntity<String> response
-                = restTemplate.postForEntity(resourceUrl, jsonObject, String.class);
-        SmsDto smsDto = new SmsDto();
-        smsDto.setBody(response.getBody());
-        //1 متن اشتباه
-        //0
-        //16 شماره موبایل اشتباه
-        System.out.println("sms:"+smsDto);
-        return smsDto;
+        // System.out.println(">>>>>>>>>>>>"+ dto.getMessage());
+        if (smsStatus) {
+            ResponseEntity<String> response
+                    = restTemplate.postForEntity(resourceUrl, jsonObject, String.class);
+            SmsDto smsDto = new SmsDto();
+            smsDto.setBody(response.getBody());
+
+            //1 متن اشتباه
+            //0
+            //16 شماره موبایل اشتباه
+            System.out.println("sms:" + smsDto);
+            return smsDto;
+        } else {
+            return null;
+        }
     }
 }
